627204e62a22d24ebf6cef30943f5f412e24857c
44x1carbon <b4079@oic.jp>
2016-02-17 01:26:39 +0900

    a

--- fuel/app/classes/controller/mtpizza/pizzadoko.php
+++ fuel/app/classes/controller/mtpizza/pizzadoko.php
@@ -13,7 +13,7 @@ class Controller_Mtpizza_Pizzadoko extends Controller_Mtpizza_Page
     {
         $user_id = Session::get(self::SESSION_KEY_USER_ID);
         $order = Model_Order::query()->where('member_id',$user_id)->where('status',false)->order_by('order_date','desc')->get_one();
-        if(!empty($order)){
+        if(empty($order)){
             return Response::redirect('mtpizza/message/pizadoko');
         } else if(!empty($order->staff_id)){
             $data['staff'] = array(
