d077743fcda2015be3aea133d922756c3de76dbd
44x1carbon <b4079@oic.jp>
2016-02-17 01:23:43 +0900

    pizzadoko

--- fuel/app/views/website/message/pizzadoko.php
+++ fuel/app/views/website/message/pizzadoko.php
@@ -1,4 +1,4 @@
 <p>現在、配達中の注文はありません</p>
 <div class="button">
-    <?php echo Html::anchor('index.php/item/list/','メニュー',array('class'=>'link'))?>
+    <?php echo Html::anchor('mtpizza/item/list/','メニュー',array('class'=>'link'))?>
 </div>
